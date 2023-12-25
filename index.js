709a4c0d75e69db207bb101bd7011d1e8e903bbc
Michael Inziani <mikeinziani004@gmail.com>
2023-12-25 15:50:23 +0300

    Add dotenv

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
+
+import 'dotenv/config'
 import express from "express";
 import bodyParser from "body-parser";
 import pg from "pg";
 
 const app = express();
-const port = 3000;
+const port = 3000 || process.env.PORT;
 
 const db = new pg.Client({
-  user: "postgres",
-  host: "localhost",
-  database: "world",
-  password: "HyperText1205Lang!&",
-  port: 5432,
+  user: process.env.Pg_user,
+  host: process.env.Pg_host,
+  password: process.env.Pg_password,
+  database: process.env.Pg_database,
+  port: process.env.Pg_port,
 });
 
 db.connect();
